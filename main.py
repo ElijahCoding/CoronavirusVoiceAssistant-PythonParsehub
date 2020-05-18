e544efeefb63aefa0f036bffebfe6936d5c0eb00
ElijahCoding <cuiyeqing960904@gmail.com>
2020-05-18 05:41:20 +0300

    get total cases, total deaths and country data

--- main.py
+++ main.py
@@ -12,12 +12,33 @@ class Data:
         self.params = {
             "api_key": self.api_key
         }
+        self.get_data()
 
     def get_data(self):
         response = requests.get(f'https://www.parsehub.com/api/v2/projects/{self.project_token}/last_ready_run/data',
                                 params=self.params)
         data = json.loads(response.text)
-        return data
+        self.data = data
+
+    def get_total_cases(self):
+        data = self.data['total']
+        for content in data:
+            if content['name'] == "Coronavirus Cases:":
+                return content['value']
+
+    def get_total_deaths(self):
+        data = self.data['total']
+        for content in data:
+            if content['name'] == "Deaths:":
+                return content['value']
+        return "0"
+
+    def get_country_data(self, country):
+        data = self.data['country']
+        for content in data:
+            if content['name'].lower() == country.lower():
+                return content
+        return "0"
 
 data = Data(api_key=API_KEY, project_token=PROJECT_TOKEN)
 print(data.get_data())
